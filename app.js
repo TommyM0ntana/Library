46369fef047ccd7be0deea15cb31ae3b15073c51
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-03-03 07:53:53 +0100

    add local storage,change the layout

--- app.js
+++ app.js
@@ -1,104 +1,152 @@
-//Book Class : rappresent a Book
-//creating a book whit the method 
+//Book class: rapresents a BOOK
+
+//start to be in use when we add a book
+//constructor method runs when we add a book
+//assign it the property(title,author and isbn) of that object using this
 class Book {
-  constructor(title, author, isbn) {
-    //passing all the paramethers asign it to the property of the object 
-    //using this. will set the title whatever the title is set to
-this.title = title 
-this.author = author
-this.isbn = isbn
+  constructor(title, author, isbn){
+  this.title = title
+  this.author = author
+  this.isbn = isbn 
+  
   }
 }
 
-
-//UI Class: Handle UI Tasks
-
+// will class contain few methods 
+//add,dipslay,remove book,show alert
+//UI class: handle UI tasks
 class UI {
-static displayBooks() {
-//hard coded array of books put it in the variable called StoredBooks
-//pretend that is a local storage
-const StoredBooks = [
-  { 
-    title:  'Functional Programming',
-    author: 'Sarah Uryelah',
-    isbn:   '3881137078'
-  },
-  {
-    title:  'BackEnd Tips',
-    author: 'Abiodun Ajidade',
-    isbn:   '377263849'
+static displayBooks(){
+  
+const books = Store.getBooks()
 
-  }
-]
-//books variable 
-const books = StoredBooks
- 
-//and then we wanna call the method addBookToList to list to UI class
-//loops throught all the array using the arrow Function whit the each method 
+
+// o function(book)
 books.forEach((book) => UI.addBookToList(book))
-  }
-//creating a method to add a book to the list
+
+//array set books to the array loop through ,call the method 
+//an passing book 
+}
+ //create the table row element tr tag
+  //to put in the tbody so #book-list  
+//get.element is the same
+
 static addBookToList(book){
-//create the row to put it into the tbody whit the id of book-list
-// get element also document.getElement
 const list = document.querySelector('#book-list')
-//create a table row element to insert the tr tag in there
+
 const row = document.createElement('tr')
-//add the colons using back ticks
+
 row.innerHTML = `
 <td>${book.title}</td>
 <td>${book.author}</td>
 <td>${book.isbn}</td>
 <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-`;
-
-//we have our list tooked from the DOM and we created new row
-//now we have to append to the row to the list
-
-
-//should call displayBooks loop and add each one to the list 
+`
 list.appendChild(row)
+  }
 
+static showAlert(message, className){
+  const div = document.createElement('div')
+  div.className = `alert alert-${className}`
+  div.appendChild(document.createTextNode(message))
+  const container = document.querySelector('.container')
+  const form = document.querySelector('#book-form')
+  container.insertBefore(div, form)
+
+  //vanish in 3 sec
+  setTimeout(() => document.querySelector('.alert').remove(),4000) 
+
+  
+}
+
+
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#isbn').value = '';
+  }
+
+static deleteBook(el) {
+  if(el.classList.contains('delete'))
+  el.parentElement.parentElement.remove()
+ }
+}
+
+class Store{
+  static getBooks(){
+let books
+if(localStorage.getItem('books') === null){
+  books = []
+} else {
+  books = JSON.parse(localStorage.getItem('books'))
+}
+return books
+  }
+  static addBook(book){
+  const books = Store.getBooks()
+  books.push(book)
+  localStorage.setItem('books', JSON.stringify(books))
+  }
+
+  static removeBook(isbn){
+   const books = Store.getBooks()
+
+   books.forEach((book, index) => {
+   if(book.isbn === isbn){
+   books.splice(index, 1)
+    }
+   } )
+
+   localStorage.setItem('books', JSON.stringify(books))
   }
 }
 
-//Store Class: Handles Storage
 
 //Event: Display Books
-
-
-//soon as the DOM loades then we are gonna call UI displaybooks
 document.addEventListener('DOMContentLoaded', UI.displayBooks)
 
+ //to submit the form and add the book to the list 
+ //should make it take all the text ad to the book-form
+ //and make it add to the book-list
 //Event: Add a book
-//to submit the book-form and add book to the list
-//we already have displayBooks from the 
-//we doing that taking it by id on this case #book-form
-//arrow function whit un event paramether
-document.querySelector('#book-form').addEventListener('submit', (e) =>{
+document.querySelector('#book-form').addEventListener('submit',(e)=> {
+   e.preventDefault()
+   //get form values
+   const title = document.querySelector('#title').value
+   const author = document.querySelector('#author').value
+   const isbn = document.querySelector('#isbn').value
+
+//validate 
+if (title === '' || author === '' || isbn === '') {
+  UI.showAlert('Please fill in all the fields','warning')
+} else {
+  //Instatiate book
+   const book = new Book(title, author, isbn)
+
+   UI.addBookToList(book)
+
+//add book to store
+
+Store.addBook(book)
 
 
-  //prevet the submit
 
+   //clear fields
 
-  e.preventDefault()
-  //get book-form values so .value
+//Show success message
+UI.showAlert('Book Added', 'success')
+   UI.clearFields()
+   }
+ })
+//remove book from the list
+document.querySelector('#book-list').addEventListener('click', (e)=>{
+  UI.deleteBook(e.target)
 
+//remove book from locale store
+Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
 
-  const title = document.querySelector('#title').value
-  const author = document.querySelector('#author').value
-  const isbn = document.querySelector('#isbn').value
-
-const book = new Book(title, author, isbn)
-
-//instantiate books
-
-//Add book to list
-
-UI.addBookToList(book)
-
-
+  UI.showAlert('Book Removed', 'primary')
 })
+//Store class : Handle Storage
 
 
-//Event:remove a book
